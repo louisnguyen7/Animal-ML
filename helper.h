2d6fb71bf89af9add801814423d5af22b9a5f37f
Louis Nguyen <louis.nguyen550@gmail.com>
2025-04-17 01:13:18 -0400

    Update helper.h

--- helper.h
+++ helper.h
@@ -1,8 +1,2 @@
-/* 
-
-© 2024 Louis Nguyen. All rights reserved. 
-
-*/
-
 /* helper function prototype - will read the test data csv file */
 int readTestData (struct Animal testData [NUM_TEST_DATA]);
